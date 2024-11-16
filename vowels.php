b3027ec5d79d9c310e3d11b95607254cc4d894ca
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-16 17:19:09 +0200

    Added linkedlist class with add function and added node class

---
+++ vowels.php
@@ -0,0 +1,36 @@
+<?php
+
+$array = json_decode($_POST['array'], true);
+
+
+class Node {
+  public $value;
+  public $next;
+
+    public function __construct($value) {
+      $this->value = $value;
+      $this->next = null;
+  }
+}
+
+
+class LinkedList {
+  public $head;
+
+  public function __construct(){
+    $this->head = null;
+  }
+
+  public function add($value) {
+    $newNode = new Node($value);
+    if($this->head == null){
+        $this->head = $newNode;
+    }else{
+        $current = $this->head;
+        while($current->next !== null){
+            $current = $current->next;
+        }
+        $current->next = $newNode;
+    }
+}
+}
