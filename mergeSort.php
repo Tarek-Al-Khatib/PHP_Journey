f7e512baca4c31398d3e64630028957329b9bbc5
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-16 16:38:37 +0200

    Added the merging function to the api

--- mergeSort.php
+++ mergeSort.php
@@ -16,3 +16,38 @@ function mergeSort($array){
 
   return merging($left, $right);
 }
+
+
+function merging($left, $right){
+  $i = 0;
+  $j = 0;
+  $sortedArray = [];
+  
+  
+  while($i < count($left) && $j < count($right)){
+    if($left[$i] > $right[$j]){
+      $result[] = $right[$j];
+      $j++;
+    }else {
+      $result[] = $left[$i];
+      $i++;
+    }
+  }
+
+
+  while ($i < count($left)) {
+    $result[] = $left[$i++];
+    $i++;
+  }
+
+  while ($j < count($right)) {
+      $result[] = $right[$j++];
+      $j++;
+  }
+
+
+  return $sortedArray;
+}
+
+
+echo json_encode(["sorted" => mergeSort($array)]);
