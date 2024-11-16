2e7498f039d90ee82d3811137a59b6dfef27b4d2
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-16 16:50:37 +0200

    Fixed many logical errors in the mergeSort

--- mergeSort.php
+++ mergeSort.php
@@ -2,52 +2,45 @@
 
 $array = json_decode($_POST['array'], true);
 
+function mergeSort($array) {
+    if(count($array) <= 1){
+        return $array;
+    }
 
-function mergeSort($array){
+    $mid = floor(count($array) / 2);
 
-  if(count($array) <= 1){
-    return $array;
-  }
-  $length = count($array);
-  $mid = $length / 2;
+    $left = mergeSort(array_slice($array, 0, $mid));
+    $right = mergeSort(array_slice($array, $mid));
 
-  $left = mergeSort(array_slice($array, 0, $mid));
-  $right = mergeSort(array_slice($array, $mid));
-  
-  return merging($left, $right);
+    return merging($left, $right);
 }
-
 
 function merging($left, $right){
-  $i = 0;
-  $j = 0;
-  $sortedArray = [];
-  
-  
-  while($i < count($left) && $j < count($right)){
-    if($left[$i] > $right[$j]){
-      $result[] = $right[$j];
-      $j++;
-    }else {
-      $result[] = $left[$i];
-      $i++;
+    $i = 0;
+    $j = 0;
+    $sortedArray = []; 
+
+    while($i < count($left) && $j < count($right)){
+        if($left[$i] <= $right[$j]){
+            $sortedArray[] = $left[$i];
+            $i++;
+        }else{
+            $sortedArray[] = $right[$j];
+            $j++;
+        }
     }
-  }
 
+    while($i < count($left)){
+        $sortedArray[] = $left[$i];
+        $i++;
+    }
 
-  while ($i < count($left)) {
-    $result[] = $left[$i++];
-    $i++;
-  }
+    while($j < count($right)){
+        $sortedArray[] = $right[$j];
+        $j++;
+    }
 
-  while ($j < count($right)) {
-      $result[] = $right[$j++];
-      $j++;
-  }
-
-
-  return $sortedArray;
+    return $sortedArray;
 }
-
 
 echo json_encode(["sorted" => mergeSort($array)]);
