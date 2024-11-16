67f5f4d091070b026a857b03d0bd65ebe7706479
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-16 17:13:10 +0200

    Fixed logical errors in palendrome

--- palendrome.php
+++ palendrome.php
@@ -1,15 +1,16 @@
 <?php
 
 $word = json_decode($_POST['word']);
-
-
 function checkPalendrome($word) {
   $length = strlen($word);
   for($i = 0; $i < $length / 2; $i++){
-    if($word[$i] != $word[$length - $i - 1]){
+    if($word[$i] !== $word[$length - $i - 1]){
       return false;
     }
   }
 
   return true;
 }
+
+
+echo json_encode(["palendrome" => checkPalendrome($word)]);
