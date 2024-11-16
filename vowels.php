b559701d29615a98ff100d55155cccf23e99f8d4
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-16 17:27:54 +0200

    Added countingVowels function

--- vowels.php
+++ vowels.php
@@ -34,3 +34,16 @@ class LinkedList {
     }
 }
 }
+
+ function countingVowels($str){
+  $vowels = ['a', 'e', 'i', 'o', 'u'];
+  $count = 0;
+  foreach(str_split(strtolower($str)) as $char){
+      if (in_array($char, $vowels)) {
+          $count++;
+      }
+  }
+  return $count;
+}
+
+
